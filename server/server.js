773f58ec2899987b0695dc201eb3368be03d961b
*Lalith <lalith889@gmail.com>
2018-05-08 10:53:41 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+
+
+const app = express();
+const publicpath = path.join(__dirname, '../public');
+
+app.use(express.static(publicpath));
+
+app.listen(3000, ()=>{
+    console.log("app started in localhost://3000");
+})
